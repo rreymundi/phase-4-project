a3610fe0db6bcf6750c84774e93a831d08af60c7
Raúl Reymundi <raul@kickstarter.com>
2023-02-15 11:30:09 -0500

    fixes broken hero image

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from '@mui/system';
 import { Typography } from '@mui/material';
+import hero from '../assets/hero.jpg'
 
 const Home = ({ user }) => {
+
   return (
       <Container sx={{ position: 'relative', color: 'white', marginTop: '48px', justifySelf: 'center' }}>
         <Container sx={{ opacity: 1, overflow: 'hidden', width: '1000px', height: '600px' }}>
-          <img src="https://github.com/rreymundi/phase-4-project/blob/main/client/src/assets/daria-shevtsova-zbWFT4eVopE-unsplash.jpg" alt="listit home" />
+          <img sx={{ objectFit: 'cover' }} src={hero} alt="collabo home" />
         </Container>
         <Container sx={{ position: 'absolute', top: '20%', left: '40%', transform: 'translate(-50%, -50%)', textAlign: 'right' }} >
           <Typography variant='h1' sx={{ width: '100%', height: '100%' }}>COLLABO</Typography>
